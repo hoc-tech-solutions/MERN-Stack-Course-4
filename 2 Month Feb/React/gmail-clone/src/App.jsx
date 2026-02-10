b1ff9e59c3e9345e1e5e3f591c4c201c253638c0
hoc-tech-solutions <hoctechsolutions@gmail.com>
2026-02-10 18:59:27 +0530

    done with changes

---
+++ 2 Month Feb/React/gmail-clone/src/App.jsx
@@ -0,0 +1,40 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+
+function App() {
+  return (
+    // React Fregment
+    <>
+      <h1 className="text-center bg-danger">HOC</h1>
+
+      <p>
+        About Us | Related Links | Site Map | Privacy Policy | Grievance Policy
+        | Refund Policy | Contact Us Use Internet Explorer Version 10.0 and
+        above. Screen resolution 1024 X 768 pixels.
+      </p>
+      <p>
+        About Us | Related Links | Site Map | Privacy Policy | Grievance Policy
+        | Refund Policy | Contact Us Use Internet Explorer Version 10.0 and
+        above. Screen resolution 1024 X 768 pixels.
+      </p>
+      <p>
+        About Us | Related Links | Site Map | Privacy Policy | Grievance Policy
+        | Refund Policy | Contact Us Use Internet Explorer Version 10.0 and
+        above. Screen resolution 1024 X 768 pixels.
+      </p>
+      <p>
+        About Us | Related Links | Site Map | Privacy Policy | Grievance Policy
+        | Refund Policy | Contact Us Use Internet Explorer Version 10.0 and
+        above. Screen resolution 1024 X 768 pixels.
+      </p>
+
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
