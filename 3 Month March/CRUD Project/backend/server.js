8be95cb1d4c75ee02cc8b980e4224e806a420567
hoc-tech-solutions <hoctechsolutions@gmail.com>
2026-03-04 19:34:49 +0530

    done with changes

--- 3 Month March/CRUD Project/backend/server.js
+++ 3 Month March/CRUD Project/backend/server.js
@@ -3,3 +3,51 @@
 // Node js 
 // Express Js
 // DB - MongoDb
+
+
+// API List
+
+// 1. API Create Item - get Data values from front end ( Items details ) and store into DB
+
+// 2. API Update Item - get Item Details from Front end which item we need to update 
+
+// 3. API Delete Item - get Item Details From Frot end an delete this record from database
+
+// 4. API get ALl Records - get All recoreds from DB and show to UI Front end
+
+
+// const getData = () => {
+
+// }
+
+// function getData(){
+    
+// }
+
+console.log("Hello Node js Project Strted")
+
+const express = require('express') // Node js Fremwork
+const app = express() // app - variable - store express function
+const mongoose = require('mongoose') // Library - connect mongodb Database
+
+
+app.use(express.json()) // convert all data into json format
+
+// DB Connection
+
+mongoose.connect("mongodb://127.0.0.1:27017/item-database").then( () => console.log("Mongo DB Connected") ).catch( (error) => console.log(error) )
+
+// Schema - Model - Data base table structure 
+// values store database - structure
+
+
+const itemsSchema = new mongoose.Schema({
+    name: String,
+    decription : String,
+    sellingPrice : Number
+})
+
+
+const Items = new mongoose.model("Items", itemsSchema) // Table Name / Collection Name - Items
+
+// API 1 - Create Item 
--- 3 Month March/CRUD Project/frontend/vite-project/src/App.jsx
+++ 3 Month March/CRUD Project/frontend/vite-project/src/App.jsx
@@ -1,18 +1,178 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import { ToastContainer, toast } from "react-toastify";
+
 import "./style.css";
+import { useState } from "react";
 
 function App() {
+  const [itemName, setItemName] = useState(); // 1 Use State Hook
+
+  console.log(itemName, "Item Name Value ");
+
+  const handleOnChange = (event) => {
+    setItemName(event.target.value);
+
+    console.log("Typing on Input field");
+  };
+
+  function SubmitForm(e) {
+    e.preventDefault();
+
+    console.log("form submitted");
+
+    toast.success("Form submitted", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+
   return (
     <>
-      <h2 className="text-danger text-center my-5">CRUD - MERN Stack Project start</h2>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      <h2 className="text-danger text-center my-5">
+        CRUD - MERN Stack Project start
+      </h2>
 
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <h3 className="border text-center">Add Item</h3>
+            <h3 className="border text-center">Create Item</h3>
+
+            <Form className="my-5">
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Item Name</Form.Label>
+
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Item Name"
+                    onChange={() => handleOnChange(event)}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>Discription</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Discription" />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Purchase Price</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter Purchase Price"
+                  />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridAddress1">
+                  <Form.Label>Selling price</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter Selling price"
+                  />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control type="number" placeholder="Enter Quantity" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Unit</Form.Label>
+                  <Form.Select defaultValue="Choose Unit">
+                    <option>Choose Unit</option>
+                    <option>Pice</option>
+                    <option>Box</option>
+                    <option>Kg</option>
+                    <option>Gram</option>
+                    <option>Litter</option>
+                  </Form.Select>
+                </Form.Group>
+              </Row>
+
+              <div className="text-center">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="w-50"
+                  onClick={SubmitForm}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Form>
           </div>
+
           <div className="col-md-6">
-            <h3 className="border text-center">Item List</h3>
+            <h3 className="border text-center">Get Items</h3>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Item Name</th>
+                  <th>Discription</th>
+                  <th>Purchase Price</th>
+                  <th>Selling price</th>
+                  <th>Quantity</th>
+                  <th>Unit</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Pen</td>
+                  <td>Jel Pen</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>Box</td>
+                  <td className="d-flex">
+                    <button className="btn btn-success"> Edit </button>
+                    <button className="btn btn-danger mx-2"> Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Book</td>
+                  <td>Note book</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>Box</td>
+                  <td className="d-flex">
+                    <button className="btn btn-success"> Edit </button>
+                    <button className="btn btn-danger mx-2"> Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
         </div>
       </div>
