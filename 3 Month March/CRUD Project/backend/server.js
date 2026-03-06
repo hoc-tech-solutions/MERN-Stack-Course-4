a00f52a648e4a19d05993119161016e12febea61
hoc-tech-solutions <hoctechsolutions@gmail.com>
2026-03-06 19:59:44 +0530

    vb

--- 3 Month March/CRUD Project/backend/server.js
+++ 3 Month March/CRUD Project/backend/server.js
@@ -1,28 +1,14 @@
 // Backed Project
-
 // Node js 
 // Express Js
 // DB - MongoDb
 
-
 // API List
-
 // 1. API Create Item - get Data values from front end ( Items details ) and store into DB
-
 // 2. API Update Item - get Item Details from Front end which item we need to update 
-
 // 3. API Delete Item - get Item Details From Frot end an delete this record from database
-
 // 4. API get ALl Records - get All recoreds from DB and show to UI Front end
 
-
-// const getData = () => {
-
-// }
-
-// function getData(){
-    
-// }
 
 console.log("Hello Node js Project Strted")
 
@@ -35,7 +21,7 @@ app.use(express.json()) // convert all data into json format
 
 // DB Connection
 
-mongoose.connect("mongodb://127.0.0.1:27017/item-database").then( () => console.log("Mongo DB Connected") ).catch( (error) => console.log(error) )
+mongoose.connect("mongodb://127.0.0.1:27017/item-database").then(() => console.log("Mongo DB Connected")).catch((error) => console.log(error))
 
 // Schema - Model - Data base table structure 
 // values store database - structure
@@ -43,11 +29,93 @@ mongoose.connect("mongodb://127.0.0.1:27017/item-database").then( () => console.
 
 const itemsSchema = new mongoose.Schema({
     name: String,
-    decription : String,
-    sellingPrice : Number
+    decription: String,
+    sellingPrice: Number
 })
 
 
 const Items = new mongoose.model("Items", itemsSchema) // Table Name / Collection Name - Items
 
 // API 1 - Create Item 
+app.post("/api/create-item", async (req, res) => {
+
+    try {
+
+        const { name, decription, sellingPrice } = req.body
+
+
+        const saveItem = new Items(
+            {
+                name,
+                decription,
+                sellingPrice
+            }
+        )
+
+        await saveItem.save()
+
+        res.status(201).json({ message : "Item Created" , data : saveItem})
+
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+// API 2 - Update/Edit Item 
+app.put("/api/update-item", (req, res) => {
+
+    try {
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+
+// API 3 - Delete Item 
+app.delete("/api/delete-item", (req, res) => {
+    try {
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+// API 4 - GetAll Item 
+app.get("/api/get-all-item", async (req, res) => {
+    try {
+
+        const items = await Items.find()
+
+        res.status(200).json({ message : "Get All Item List", data : items})
+
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+// Helth API
+app.get("/helth", (req, res) => {
+    res.status(200).json({ message: "Server is Runing" })
+})
+
+
+
+// server Start
+const PORT = 9090
+
+app.listen(PORT, () => {
+    console.log('Server Started')
+})
